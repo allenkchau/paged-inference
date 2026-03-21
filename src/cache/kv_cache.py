a368d8e74e16705803625f8713cd3b778c2224ff
Allen Chau <achau7704@gmail.com>
2026-03-21 16:16:20 -0700

    start kv cache implementation

--- src/cache/kv_cache.py
+++ src/cache/kv_cache.py
@@ -3,8 +3,23 @@ import torch
 
 class KVCache:
     def __init__(self):
-        pass
+        self.cache = # backend cache structure?
 
-    # append a new decode token to the cache
-    def append(self, ):
-        self.
+        # how many tokens are currently cached
+        self.cur_len = 0
+
+    # initialize our cache during model prefill phase
+    def initialize_cache(self, ):
+        self.cache = 
+        
+
+    # update the cache from the decode phase
+    def update_cache(self, new_token_id):
+
+        # we added a token to the cache
+        self.cur_len += 1
+
+    # get the length of our cache
+    # this method exists in addition to cur_len attribute because 
+    def get_cache_length(self):
+        return self.cur_len
--- src/engine/request.py
+++ src/engine/request.py
@@ -1,24 +1,38 @@
+
 class Request:
-    def __init__(self, request_id, prompt_text, prompt_token_ids, generated_token_ids, max_new_tokens, finished=False):
+    def __init__(self, request_id, prompt_text, prompt_token_ids: list[int], max_new_tokens, eos_token_id):
         # identity
         self.request_id = request_id
 
         # input/output content
+        # we store the token ids in the request object as Python lists
         self.prompt_text = prompt_text
         self.prompt_token_ids = prompt_token_ids
-        self.generated_token_ids = generated_token_ids
+        self.generated_token_ids: list[int] = []
 
         # generation constraints
         self.max_new_tokens = max_new_tokens
+        self.eos_token_id = eos_token_id
 
         # status
-        self.finished = finished
+        self.finished = False
 
-    def get_prompt_length(self, ):
-        pass
+    def get_prompt_length(self) -> int:
+        return len(self.prompt_token_ids)
 
-    def get_generated_length(self, ):
-        pass
+    def get_generated_length(self) -> int:
+        return len(self.generated_token_ids)
+
+    def get_total_length(self) -> int:
+        return len(self.prompt_token_ids) + len(self.generated_token_ids)
+
+    def get_total_token_ids(self) -> list[int]:
+        return self.prompt_token_ids + self.generated_token_ids
+
+    def append_token(self, new_token: int):
+        self.generated_token_ids.append(new_token)
+        if new_token == self.eos_token_id or self.get_generated_length() == self.max_new_tokens:
+            self.finished = True
 
     
 
